b6c4113528fd76a488f22f4d7bb141aada8781c2
Ulfasartika <ulfasartika005@gmail.com>
2023-11-13 19:55:58 +0700

    create kategori

--- assets/js/kategori/input_kategori.js
+++ assets/js/kategori/input_kategori.js
@@ -1,19 +1,17 @@
-
 $('button').on('click', function() {
-    var kategori = $('input[name="nama_kategori"]').val();
+    var kategori = $('input[name="kategori"]').val();
 
     $.ajax({
-        type: 'POST',
-        url: "http://localhost/e-commerce/dummy/input_kategori.php",
+        type:'POST',
+        url:"http://e-commerce.test/dummy/input_kategori.php",
         data: {
             nama_kategori: kategori
         },
         dataType: 'json',
-        async: false,
+        async: true,
         success: function(result){
-           console.log(result);
+            console.log(result);
+            window.location.replace("http://e-commerce.test/kategori.html");
         }
-    });
-
-})
-
+    }); 
+});
